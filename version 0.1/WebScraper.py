75be95955d83656812dd0e5cda3cb55e8a1ff1aa
Joshua Taylor <joshuaat@bu.edu>
2020-09-17 17:34:39 -0400

    Version 0.1 includes beginnings of api functionality and xlsx interaction

---
+++ version 0.1/WebScraper.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 24 19:04:29 2020
+
+@author: josh
+"""
+
+from openpyxl import Workbook
+import os
+import datetime
+import time
+import tkinter as tk
+#path = os.getcwd()
+#files = []
+#
+#
+#for r,d,f in os.walk(path):
+#    for file in f:
+#        if('.xlsx' in file or '.xls' in file):
+#            files.append(file)
+window = tk.Tk()
+window.mainloop()
+greeting = tk.Label(text = "fuck you bert")
+greeting.pack()
+window.mainloop()
